944ef80f107196dd7808e5176ddff78fc90fbeaa
Thanaphat Sanngoen <blazesonic14@gmail.com>
2024-06-28 12:24:09 +0700

    commit 5.5

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ createApp({
         const image = ref ('./assets/images/socks_green.jpg')
         const inStock = ref(true)
         const inventory = ref(100)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const sizes = ref([
+            'size S',
+            'size M',
+            'size L'
+        ])
+        const variants = ref([
+            {id: 2234, color: 'green'},
+            {id: 2235, color: 'blue'}
+        ])
         const description =ref ("greats socks")
         const onSale = ref(true)
         return{
@@ -15,6 +29,9 @@ createApp({
             link:"https://www.camt.cmu.ac.th/index.php/th/" ,
             inStock,
             inventory,
+            details,
+            variants,
+            sizes,
             onSale
         }
     }
