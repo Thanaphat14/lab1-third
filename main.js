8c98e857111861f56f937ff4c567b1bca5b99553
thanaphat <thanaphat_sanngoen@cmu.ac.th>
2024-06-28 10:55:53 +0700

    Commit 4.9

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ createApp({
     setup(){
         const product = ref ('Boots')
         const image = ref ('./assets/images/socks_green.jpg')
+        const inStock = ref(true)
+        const inventory = ref(100)
         const description =ref ("greats socks")
+        const onSale = ref(true)
         return{
             product,
             image,
             description,
-            link:"https://www.camt.cmu.ac.th/index.php/th/"
+            link:"https://www.camt.cmu.ac.th/index.php/th/" ,
+            inStock,
+            inventory,
+            onSale
         }
     }
 
